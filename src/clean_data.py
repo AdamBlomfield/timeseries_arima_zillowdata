fd46a82772329995613e6239fdf28380b4f097d1
D. Ryan Miller <d.ryanmiller542@gmail.com>
2019-07-12 13:28:26 -0400

    final notebook

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,5 +1,76 @@
-# clean_data.py
+# Import and filter data
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller 
+import numpy as np
+import matplotlib.pyplot as plt
 
-def test_clean_data():
-    print("In clean_data")
-    return None 
+
+def detrend_test(TS, alpha=0.05, maxlag=4, suppress_output=False):
+    '''Selecting the best method for detrending timeseries based on lowest p-value of the augmented Dickey-Fuller.
+       TS: timeseries dataframe
+       alpha: alpha value for Dickey-Fuller '''
+    
+    new_TS = pd.DataFrame()
+    
+    plist = []  
+    plist_zips = []
+    
+    # Keep track of which zipcodes require a log 1st difference transformation
+    log_1diff = []
+    
+    for column in list(TS.columns):  #go through each zipcode in the DF
+        p_values = []
+        
+        # First Difference
+            # find the first difference for each row in the zipcode
+        data_1diff = TS[column].diff(periods=1) 
+            # perform Dickey Fuller test on first difference
+        dftest = adfuller(data_1diff.dropna(),maxlag=maxlag)
+            # Place first 4 outputs of the Dickey Fuller test in a dataframe and label the outputs appropriately in the index
+        dfoutput_1diff = pd.Series(dftest[0:4], index=['Test Statistic', 'p-value', '#Lags Used', 'Number of Observations Used'])
+        for key, value in dftest[4].items():
+            dfoutput_1diff['Critical Values {}'.format(key)] = value
+            # Add p-value of first difference Dickey-Fuller p-value list
+        p_values.append(dfoutput_1diff[1])
+    
+        # Log First Difference
+            # find the log first difference for each row in the zipcode
+        data_log_1diff = TS[column].apply(lambda x: np.log(x)) - TS[column].apply(lambda x: np.log(x)).shift(1)
+            # perform Dickey Fuller test on first difference
+        dftest = adfuller(data_log_1diff.dropna(),maxlag=maxlag)
+            # Take first 4 outputs of the Dickey Fuller test and label the outputs appropriately
+        dfoutput_log_1diff = pd.Series(dftest[0:4], index=['Test Statistic', 'p-value', '#Lags Used', 'Number of Observations Used'])
+        for key, value in dftest[4].items():
+            dfoutput_log_1diff['Critical Values {}'.format(key)] = value
+            # Add p-value of log first difference Dickey-Fuller p-value list
+        p_values.append(dfoutput_log_1diff[1])
+
+        # If first difference performed better, print the Dickey-Fuller results and plot
+        if np.argmin(p_values)==0:
+            data_1diff.plot(figsize=(20,6))
+            plt.title('{} First Difference'.format(column))
+            plt.show();
+            print(dfoutput_1diff)
+            new_TS[column]=data_1diff
+        
+        # If log first difference performed better, print the Dickey-Fuller results and plot
+        elif np.argmin(p_values)==1:
+            log_1diff.append(column)
+            data_log_1diff.plot(figsize=(20,6))
+            plt.title('{} Log First Difference'.format(column))
+            plt.show();
+            print(dfoutput_log_1diff)
+            new_TS[column]=data_log_1diff
+        
+        # Add the smallest p value from tests, to the plist
+        plist.append(min(p_values))
+        # Add zipcodes with high p-values to plist_zips
+        if min(p_values)>alpha:
+            plist_zips.append(column)
+    
+    
+    if suppress_output==False:
+        print('\nNumber of p-values above alpha of {}:'.format(alpha),(np.array(plist)>alpha).sum())
+        print('\nZipcodes with p-values above alpha of {}'.format(alpha), plist_zips)
+        print('\nZipcodes requiring log first difference transformation: {}'.format(log_1diff))
+        return new_TS, log_1diff
--- src/model.py
+++ src/model.py
@@ -1,4 +1,88 @@
+from statsmodels.tsa.arima_model import ARIMA
+from datetime import datetime 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 
-def test_model():
-    print('In Model')
-    pass
+def evaluate_arima_model(X, test, arima_order):
+    '''Provides the Mean Squared Error for the train vs test
+    arima_order = configuration of (p,d,q)'''
+    # prepare training dataset
+    history = [x for x in X]
+    # make predictions
+    predictions = []
+    for t in range(len(test)):
+        model = ARIMA(np.array(X.dropna()), order=arima_order)
+        model_fit = model.fit(disp=0)
+        yhat = model_fit.forecast()[0]
+        predictions.append(yhat)
+        history.append(test[t])
+    # calculate out of sample error
+    mse = mean_squared_error(test, predictions)
+    return mse
+
+
+def evaluate_models(train_column, test, p_values, d_values, q_values):
+    best_pdq, best_score = None, float('inf')
+    for p in p_values:
+        for d in d_values:
+            for q in q_values:
+                arima_order = (p,d,q)
+                try:
+                    rmse = np.sqrt(evaluate_arima_model(train_column, test, arima_order))
+                    if rmse < best_score:
+                        best_pdq, best_score = arima_order, rmse
+                except:
+                    continue
+    print('Best ARIMA {} RMSE= {}'.format(best_pdq, best_score))
+    return best_pdq
+
+
+
+def predict_arima_model(data, column, arima_order, periods):
+    '''Output: Final predicted median price for zipcode, the zipcode's price growth in absolute terms as well as %, 
+    the risk associated with the zipcode investment and finally a prediction chart to illustrate the 
+    prediction intervals'''
+    
+    # Perform ARIMA model on the chosen Zipcode
+    model = ARIMA(np.array(data[column].dropna()), order=arima_order)
+    res = model.fit(disp=0)
+    pred= res.forecast(steps=periods)
+    
+    from dateutil.relativedelta import relativedelta
+    # Extend the date range of the dataframe
+    start = datetime.strptime("2017-11-01", "%Y-%m-%d")
+    date_list = [start + relativedelta(months=x) for x in range(0,periods)]
+    future = pd.DataFrame(data=pred[0], index=date_list)
+    complete= pd.concat([data[column], future])
+    
+    # plot the data, prediction and confidence intervals
+        # Data
+    plt.plot(data[column])
+        # Prediction
+    plt.plot(future, color='red', lw=5, ls='--')
+        # Confidence Intervals
+    lower = [pred[2][x][0] for x in range(len(pred[2]))]
+    lower_bound = pd.DataFrame(data=lower, index=date_list)
+    upper = [pred[2][x][1] for x in range(len(pred[2]))]
+    upper_bound = pd.DataFrame(data=upper, index=date_list)
+    plt.fill_between(upper_bound.index, upper, lower, color='r', alpha=.3)
+    
+    # Make the chart title and axis labels
+    plt.xlabel('Time (years)')
+    plt.ylim(np.min(data[column])-50000,np.max(data[column])+150000)
+    plt.ylabel('Median Housing Price')
+    plt.title('Median House Price Prediction for Zipcode {}'.format(column))
+    plt.show();
+    
+    # Print out Final Price, Growth ($ and %) and the associated risk
+    final_price = future[0][-1]
+    growth_level = final_price - data[column][-1]
+    growth_perc = ((final_price - data[column][-1])/data[column][-1])*100
+    risk = ((upper[-1]-lower[-1])/lower[-1])*100
+    print(' Final Predicted Price: ${}'.format(round(final_price),0),'\n',
+          'Total Growth: ${}'.format(round(growth_level,0)),'\n',
+          'Percentage Growth: {}%'.format(round(growth_perc,2)),'\n',
+          'Risk: {}%'.format(round(risk,2)))
+    return pred
